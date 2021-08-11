ac952ed231618731cbf111346d11145ceada715b
Sachinsadhvik <sachindagrt88@gmail.com>
2021-08-11 23:36:28 +0530

    changed file

--- qodserver.js
+++ qodserver.js
@@ -6,8 +6,8 @@ port= 5000 || process.env.PORT;
 app.set('views','VIEW');
 app.set('view engine',"ejs");
 app.get('/',(req,res)=> {
-getA().then(received=>{ //var result= received.data.contents.quotes;
-        //for(items in result)
+getA().then(received=>{ var result= received.data.contents.quotes;
+        for(items in result)
         {   res.render("QUOTE",{
             quote:"The human mind and body are truly extraordinary. They are the quintessence of excellence in motion. We talk, touch, see, hear, taste, smell, and feel. We dream, aspire, and become. All that we are is mind and body and spirit-that is our universe."       
             ,author: "Lorii Myers"//result[items].author
@@ -17,11 +17,8 @@ getA().then(received=>{ //var result= received.data.contents.quotes;
  
 
 })
-
-
-console.log("hello")
 async function getA() {
-//const result = await axios.get("https://quotes.rest/qod");
+const result = await axios.get("https://quotes.rest/qod");
 return result="fsds";
 }
 
