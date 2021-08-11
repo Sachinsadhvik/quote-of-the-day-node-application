e652733af55b578caee77926fda8144ad8bb645b
Sachinsadhvik <sachindagrt88@gmail.com>
2021-08-11 23:29:08 +0530

    added start to package.json

--- qodserver.js
+++ qodserver.js
@@ -2,14 +2,15 @@
 const express = require('express');
 const axios = require('axios');
 var app = express();
+port= 5000 || process.env.PORT;
 app.set('views','VIEW');
 app.set('view engine',"ejs");
 app.get('/',(req,res)=> {
-getA().then(received=>{ var result= received.data.contents.quotes;
-        for(items in result)
+getA().then(received=>{ //var result= received.data.contents.quotes;
+        //for(items in result)
         {   res.render("QUOTE",{
-            quote:result[items].quote,
-            author:result[items].author
+            quote:"The human mind and body are truly extraordinary. They are the quintessence of excellence in motion. We talk, touch, see, hear, taste, smell, and feel. We dream, aspire, and become. All that we are is mind and body and spirit-that is our universe."       
+            ,author: "Lorii Myers"//result[items].author
         })
         }
 }).catch(err=>{ console.log(err)})
@@ -20,11 +21,11 @@ getA().then(received=>{ var result= received.data.contents.quotes;
 
 console.log("hello")
 async function getA() {
-const result = await axios.get("https://quotes.rest/qod");
-return result;
+//const result = await axios.get("https://quotes.rest/qod");
+return result="fsds";
 }
 
-port= 5000 || process.env.PORT;
 
-app.listen(5000, ()=>{console.log("server started")});
+
+app.listen(port, ()=>{console.log("server started")});
 
